85685c803258cf58ea1688f23d7329f9562971f1
ArpitExpound <arpit.tripathi@expoundtechnivo.com>
2025-09-22 09:56:46 +0530

    enter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 
+const title='Indecision';
+const subtitle='Put your life in the hands of a computer';
+const options=['Thing one', 'Thing two', 'Thing four'];
+
 class IndecisionApp extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle}/>
         <Action />
-        <Options />
+        <Options options={options}/>
+        <Option/>
+        <AddOption/>
       </div>
     );
   }
@@ -16,28 +22,72 @@ class IndecisionApp extends React.Component {
     render() {
       return (
         <div>
-          <h1>Indecision</h1>
-          <h2>Put your life in the hands of a computer</h2>
+          <h1>{this.props.title}</h1>
+          <h2>{this.props.subtitle}</h2>
         </div>
       );
     }
   }
 
   class Action extends React.Component {
+    handlePick() {
+      alert('handlePick');
+    }
     render() {    
       return (
         <div>
-          <button>What should I do?</button>
+          <button onClick={this.handlePick}>What should I do?</button>
         </div>
       );
     }
   }
 
   class Options extends React.Component {
+    
+    handleRemoveAll() {
+      alert('handleRemoveAll');
+    }
+
     render() {    
       return (
         <div>
-          Options Component here
+          <button onClick={this.handleRemoveAll}>Remove All</button>
+          {this.props.options.map((option) => <Option key={option} optionText={option}/>)}   
+        </div>
+      );
+    }
+  }
+
+  class Option extends React.Component {
+    render() {    
+      return (
+        <div>
+          {this.props.optionText}
+        </div>
+      );
+    }
+  }
+
+  class AddOption extends React.Component {
+    
+    onFormSubmit(e) {
+      e.preventDefault();
+
+      const option = e.target.elements.option.value.trim();
+
+      if (option) {
+        alert(option);
+      }
+
+      e.target.elements.option.value = '';
+    } 
+    render() {    
+      return (
+        <div>
+          <form onSubmit={this.onFormSubmit}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+          </form>
         </div>
       );
     }
